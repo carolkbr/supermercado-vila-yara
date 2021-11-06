beca7c1011aeb9a95aa10457ed12e3bd6396c060
Caroline Rolim <ckbrolim@gmail.com>
2021-11-06 17:51:04 -0300

    Primeiro commit

---
+++ vila-yara/src/main/java/com/grupo3/vilayara/VilaYaraApplication.java
@@ -0,0 +1,13 @@
+package com.grupo3.vilayara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VilaYaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VilaYaraApplication.class, args);
+	}
+
+}
---
+++ vila-yara/src/test/java/com/grupo3/vilayara/VilaYaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo3.vilayara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VilaYaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
